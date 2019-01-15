b82fdc8364d72bc590ad8da3d79f48efa450c176
28JayG <jaygandhi.32@outlook.com>
2019-01-15 16:29:54 +0530

    * Adds AppBar * Adds BottomBar * Creates the buildTextButton

---
+++ lib/widgets/build_text_button.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+//TODO:(1.2) build your text button
+//Widget buildTextButton({String title, bool isOnLight, BuildContext context}){
+//  return FlatButton(
+//    padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 15.0),
+//    child: Text(
+//      title.toUpperCase(),
+//      style: TextStyle(
+//        color: isOnLight? Theme.of(context).primaryColor : Colors.white,
+//        fontSize: 14.0,
+//        fontWeight: FontWeight.bold,
+//      ),
+//    ),
+//    onPressed: (){},
+//  );
+//}
